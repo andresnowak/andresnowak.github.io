ca44c70b7bfe205dc032bf8cd641ae55f2d187f5
NKspartan <andresnowak52@gmail.com>
2022-04-29 13:32:28 -0500

    extras

--- svelte.config.js
+++ svelte.config.js
@@ -6,14 +6,17 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({ fallback: 'index.html' }),
 
 	kit: {
 		adapter: adapterStatic({
 			pages: 'docs',
 			assets: 'docs',
 			fallback: null
-		})
+		}),
+		prerender: {
+			default: true
+		}
 	},
 
 	paths: {
